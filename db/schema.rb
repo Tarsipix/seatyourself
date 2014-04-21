a8a28c01fb2542862f4eaf8991127116a27282ac
Brian Simon <tarsipix@gmail.com>
2014-04-21 13:46:46 -0400

    Switched from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,48 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417204403) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "reservations", force: true do |t|
-    t.datetime "time"
-    t.integer  "customer_id"
-    t.integer  "restaurant_id"
-    t.integer  "party_size"
-    t.integer  "min_seats"
-    t.integer  "max_seats"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "restaurants", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "address"
-    t.string   "phone"
-    t.string   "photo_url"
-    t.integer  "category_id"
-    t.integer  "owner_id"
-    t.integer  "capacity"
-    t.time     "open_from"
-    t.time     "open_to"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.integer  "points"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "user_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
